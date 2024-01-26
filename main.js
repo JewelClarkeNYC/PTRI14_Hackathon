a2be672f514ba2e0b1a09d7366e85d3c7b33e100
Jewel Clarke <133067242+JewelClarkeNYC@users.noreply.github.com>
2024-01-26 00:30:29 -0500

    initial pseudo code and some preliminary logic

---
+++ main.js
@@ -0,0 +1,26 @@
+// Create a function PopUp() that makes a popup window pop up on the user's browser every 45 min if extension is active
+    //PopUp() will invoke activitySelector()  
+// Create a prompt activitySelector giving user choice of stretch, hydration, extended break, or meditation 
+const activitySelector = prompt(`"Hey hard worker! What time is it?" Type: stretch, drink, extended break, or meditation`,[]);
+    // if stretch selected, invokes stretchSelector()
+    if(activitySelector === stretch){
+        stretcSelector();
+    } else if(activitySelector === drink){
+    // if hydration selected, 
+
+    } else if(activitySelector === extended break){
+    // if extended break selected, puts extension timer on pause
+
+    } else (activitySelector === meditation){
+    // if meditation selecting, starts meditation video
+
+    }
+// Create a function stretchSelector()
+    //when invoked, posts random stretching image from array(?) for 5 min
+// Create a function timeToEat() that runs every day at 9am, 12pm, 3pm, 6pm
+    //at 9am flashes breakfast.png
+    //at 12pm flashes lunch.png
+    //at 3pm flashes snacktime.png
+    //at 6pm flashes dinner.png   
+// Create a function  
+// upon invocation, 
